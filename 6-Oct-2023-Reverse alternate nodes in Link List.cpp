75e4189b0493b433ff77972d26b0986250d43680
gprs1022 <pradeep1022gprs@gmail.com>
2023-10-06 07:13:18 +0530

    Reverse alternate nodes in Link List

---
+++ 6-Oct-2023-Reverse alternate nodes in Link List.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    void rearrange(struct Node *odd)
+    {
+        //add code here
+        if(odd == NULL || odd->next == NULL || ((odd->next)->next) == NULL)
+            return;
+        
+        struct Node* trev = NULL;
+        struct Node * temp = odd;
+        
+        while((temp->next)!=NULL){
+            Node* ptem = temp->next;
+            if(ptem->next == NULL){
+                ptem->next = trev;
+                trev = ptem;
+                break;
+            }
+            if((ptem->next) != NULL){
+                temp->next = ptem->next;
+                temp = temp->next;
+            }
+            ptem->next = trev;
+            trev = ptem;
+        }
+        temp->next = trev;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
