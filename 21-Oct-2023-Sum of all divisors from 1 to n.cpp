16383e53e5c495c628966adc53c4a0a8b0c6c7da
gprs1022 <pradeep1022gprs@gmail.com>
2023-10-21 09:36:20 +0530

    sum of all divisors from 1 to n

---
+++ 21-Oct-2023-Sum of all divisors from 1 to n.cpp
@@ -0,0 +1,14 @@
+//sum of all divisors from 1 to n 
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        // Write Your Code here
+        long long int ans = 0;
+       for(int i=1;i<=N;i++){
+           ans+=(N/i)*i;
+       }
+       return ans;
+    }
+};
